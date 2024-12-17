a00587a1cf542e98642263f09085a0487b96156b
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-12-16 20:06:15 -0800

    nit commit

---
+++ CollageNeue/CollageNeueApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct CollageNeueApp: App {
+  var body: some Scene {
+    WindowGroup {
+      MainView().environmentObject(CollageNeueModel())
+    }
+  }
+}
---
+++ CollageNeue/Model/CollageNeueModel.swift
@@ -0,0 +1,71 @@
+import UIKit
+import Photos
+
+class CollageNeueModel: ObservableObject {
+  static let collageSize = CGSize(width: UIScreen.main.bounds.width, height: 200)
+  
+  // MARK: - Collage
+  
+  private(set) var lastSavedPhotoID = ""
+  private(set) var lastErrorMessage = ""
+
+  func bindMainView() {
+    
+  }
+
+  func add() {
+    
+  }
+
+  func clear() {
+    
+  }
+
+  func save() {
+    
+  }
+  
+  // MARK: -  Displaying photos picker
+  private lazy var imageManager = PHCachingImageManager()
+  private(set) var thumbnails = [String: UIImage]()
+  private let thumbnailSize = CGSize(width: 200, height: 200)
+
+  func bindPhotoPicker() {
+    
+  }
+  
+  func loadPhotos() -> PHFetchResult<PHAsset> {
+    let allPhotosOptions = PHFetchOptions()
+    allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+    return PHAsset.fetchAssets(with: allPhotosOptions)
+  }
+
+  func enqueueThumbnail(asset: PHAsset) {
+    guard thumbnails[asset.localIdentifier] == nil else { return }
+    
+    imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+      guard let image = image else { return }
+      self.thumbnails[asset.localIdentifier] = image
+    })
+  }
+  
+  func selectImage(asset: PHAsset) {
+    imageManager.requestImage(
+      for: asset,
+      targetSize: UIScreen.main.bounds.size,
+      contentMode: .aspectFill,
+      options: nil
+    ) { [weak self] image, info in
+      guard let self = self,
+            let image = image,
+            let info = info else { return }
+      
+      if let isThumbnail = info[PHImageResultIsDegradedKey as String] as? Bool, isThumbnail {
+        // Skip the thumbnail version of the asset
+        return
+      }
+      
+      // Send the selected image
+    }
+  }
+}
